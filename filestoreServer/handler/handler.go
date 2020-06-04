3c6769d2b7e6224a2d3b95bf87cb9a93f441fe35
audiRStony <jzb89757@sina.com>
2020-06-04 23:27:17 +0800

    aaa

--- filestoreServer/handler/handler.go
+++ filestoreServer/handler/handler.go
@@ -7,11 +7,15 @@ package handler
 @Software: GoLand
 */
 import (
+	"encoding/json"
+	"filestoreServer/meta"
+	"filestoreServer/util"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
 //处理文件上传
@@ -32,18 +36,30 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		defer file.Close()
-		newfile, err := os.Create("/tmp/" + head.Filename)
+
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "/tmp/" + head.Filename,
+			UploadAt: time.Now().Format("2006-01-02 15:04:05"),
+		}
+
+		newFile, err := os.Create(fileMeta.Location)
 		if err != nil {
 			fmt.Printf("Create file failed,err %s\n", err.Error())
 			return
 		}
-		defer newfile.Close()
+		defer newFile.Close()
 
-		_, err = io.Copy(newfile, file)
+		fileMeta.FileSize, err = io.Copy(newFile, file)
 		if err != nil {
 			fmt.Printf("Failed to save data into file,err: %s\n", err.Error())
 			return
 		}
+
+		newFile.Seek(0, 0)
+		fileMeta.FileSha1 = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
+
 		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
 }
@@ -51,4 +67,80 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 //上传已完成
 func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Upload finished!")
+}
+
+//获取文件元数据
+func GetFileMetaHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	filehash := r.Form["filehash"][0]
+	fMeta := meta.GetFileMeta(filehash)
+	data, err := json.Marshal(fMeta)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
+
+//文件下载
+func DownloadHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fsha1 := r.Form.Get("filehash")
+	fm := meta.GetFileMeta(fsha1)
+
+	f, err := os.Open(fm.Location)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError) //给前端返回错误码
+		return
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f) //将读到的文件加载到内存
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/octect-stream")
+	w.Header().Set("Content-Descrption", "attachment;filename=\""+fm.FileName+"\"")
+	w.Write(data)
+}
+
+//更新元信息接口(重命名)
+func FileMetaUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	opType := r.Form.Get("op")
+	filesha1 := r.Form.Get("filehash")
+	newFileName := r.Form.Get("filename")
+
+	if opType != "0" {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	curFileMeta := meta.GetFileMeta(filesha1)
+	curFileMeta.FileName = newFileName
+	meta.UpdateFileMeta(curFileMeta)
+
+	w.WriteHeader(http.StatusOK)
+	data, err := json.Marshal(curFileMeta)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+//删除文件及元信息
+func FileDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fileSha1 := r.Form.Get("filehash")
+	fMeta := meta.GetFileMeta(fileSha1)
+	os.Remove(fMeta.Location) //通过os删除本地文件
+	/*暂时忽略报错*/
+	meta.RemoveFileMeta(fileSha1) /*暂时认为删除元信息就表示删除成功*/
+	w.WriteHeader(http.StatusOK)
 }
--- filestoreServer/meta/filemeta.go
+++ filestoreServer/meta/filemeta.go
@@ -1,5 +1,9 @@
 package meta
 
+import (
+	"sort"
+)
+
 /*
 @Time : 2020/5/27 11:21 下午
 @Author : audiRS7
@@ -14,4 +18,36 @@ type FileMeta struct {
 	FileSize int64
 	Location string
 	UploadAt string
+}
+
+var fileMetas map[string]FileMeta
+
+func init() {
+	fileMetas = make(map[string]FileMeta)
+}
+
+//新增/更新文件元信息
+func UpdateFileMeta(fmeta FileMeta) {
+	fileMetas[fmeta.FileSha1] = fmeta
+}
+
+//通过sha1值获取文件元信息对象
+func GetFileMeta(fileSha1 string) FileMeta {
+	return fileMetas[fileSha1]
+}
+
+//获取批量的文件元信息列表
+func GetLastFileMetas(count int) []FileMeta {
+	fMetaArray := make([]FileMeta, len(fileMetas))
+	for _, v := range fileMetas {
+		fMetaArray = append(fMetaArray, v)
+	}
+
+	sort.Sort(ByUploadTime(fMetaArray))
+	return fMetaArray[0:count]
+}
+
+//删除元信息
+func RemoveFileMeta(fileSha1 string) {
+	delete(fileMetas, fileSha1)
 }
